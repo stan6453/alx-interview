debf218df374c19e92eaaa3d9f37c5497a4098e8
stanley <stanley6453@gmail.com>
2023-07-14 01:33:15 +0100

    created file 0x02-minimum_operations/0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single character H.
+Your text editor can execute only two operations in this file:
+Copy All and Paste. Given a number n, write a method that
+calculates the fewest number of operations needed to result
+in exactly n H characters in the file.
+"""
+
+
+def minOperations(n):
+    """
+    Given a number n, write a method that calculates the
+    fewest number of operations needed to result
+    in exactly n H characters in the file.
+    """
+    sum_factors = 0
+    factor = 1
+
+    while True:
+        factor = factor + 1
+        if n % factor == 0:
+            sum_factors += factor
+            n = n/factor
+            factor = 1
+            if n == 1:
+                return sum_factors
