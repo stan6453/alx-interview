e8ede168379f83c81a3e620e4bde1d7b9a3cf9b4
stanley <stanley6453@gmail.com>
2023-07-20 16:10:39 +0100

    created file 0x03-log_parsing/0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,10 +20,10 @@ total_file_size = 0
 
 
 def print_stats():
-    print("File size: {}".format(total_file_size))
+    print("File size: {:d}".format(total_file_size))
     for key, value in status_codes.items():
         if value:
-            print("{}: {}".format(key, value))
+            print("{}: {:d}".format(key, value))
 
 
 log_pattern = (r'^(\d+\.\d+\.\d+\.\d+) - \[(\d{4}-\d{2}-\d{2} '
@@ -38,15 +38,21 @@ try:
         if re.match(log_pattern, line):
             method = str(array[-5][1:])
             status_code = str(array[-2])
-            file_size = int(array[-1])
-            if method == "GET" and status_code.isnumeric():
-                # calc stats
-                total_file_size += file_size
-                status_codes[status_code] += 1
-                line_count += 1
+            try:
+                file_size = int(array[-1])
+            except:
+                pass
+            # calc stats
+            total_file_size += file_size
+            try:
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+            except:
+                pass
+            line_count += 1
 
-                if (line_count % 10 == 0):
-                    print_stats()
+            if (line_count % 10 == 0):
+                print_stats()
 except KeyboardInterrupt:
     print_stats()
     raise
