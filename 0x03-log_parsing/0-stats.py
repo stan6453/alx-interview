95e51ac89f19b49809118fffe39859ffbe1d9f38
stanley <stanley6453@gmail.com>
2023-07-20 15:42:16 +0100

    created file 0x03-log_parsing/0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,6 +2,7 @@
 """Parse a log file"""
 
 import sys
+import signal
 import re
 
 status_codes = {
@@ -31,23 +32,22 @@ log_pattern = (r'^(\d+\.\d+\.\d+\.\d+) - \[(\d{4}-\d{2}-\d{2} '
                r'([a-zA-Z0-9]+) (\d+)$'
                )
 
-
-try:
-    for line in sys.stdin:
-        line = line[:-1]
-        array = line.split()
-        if re.match(log_pattern, line):
-            method = str(array[-5][1:])
-            status_code = str(array[-2])
-            file_size = int(array[-1])
-            if method == "GET" and status_code.isnumeric():
-                # calc stats
-                total_file_size += file_size
-                status_codes[status_code] += 1
-                line_count += 1
-
-                if (line_count % 10 == 0):
-                    print_stats()
-except KeyboardInterrupt:
+def handle_sigint(signal, frame):
     print_stats()
-    sys.exit(0)
+signal.signal(signal.SIGINT, handle_sigint)
+
+for line in sys.stdin:
+    line = line[:-1]
+    array = line.split()
+    if re.match(log_pattern, line):
+        method = str(array[-5][1:])
+        status_code = str(array[-2])
+        file_size = int(array[-1])
+        if method == "GET" and status_code.isnumeric():
+            # calc stats
+            total_file_size += file_size
+            status_codes[status_code] += 1
+            line_count += 1
+
+            if (line_count % 10 == 0):
+                print_stats()
