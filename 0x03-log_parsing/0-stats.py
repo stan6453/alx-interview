bd68e84f0403268432a2a2d064e3b39ec7c1fc9d
stanley <stanley6453@gmail.com>
2023-07-20 16:06:00 +0100

    created file 0x03-log_parsing/0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,52 @@
 #!/usr/bin/python3
-
-"""Script that reads stdin line by line and computes metrics"""
+"""Parse a log file"""
 
 import sys
+import re
+
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+
+line_count = 0
+total_file_size = 0
 
 
-def printsts(dic, size):
-    """ WWPrints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+def print_stats():
+    print("File size: {}".format(total_file_size))
+    for key, value in status_codes.items():
+        if value:
+            print("{}: {}".format(key, value))
 
 
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
+log_pattern = (r'^(\d+\.\d+\.\d+\.\d+) - \[(\d{4}-\d{2}-\d{2} '
+               r'\d{2}:\d{2}:\d{2}\.\d+)\] "(\w+ [^"]+)" '
+               r'([a-zA-Z0-9]+) (\d+)$'
+               )
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
+        line = line[:-1]
+        array = line.split()
+        if re.match(log_pattern, line):
+            method = str(array[-5][1:])
+            status_code = str(array[-2])
+            file_size = int(array[-1])
+            if method == "GET" and status_code.isnumeric():
+                # calc stats
+                total_file_size += file_size
+                status_codes[status_code] += 1
+                line_count += 1
 
-        stlist = line.split()
-        count += 1
-
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
-
-
+                if (line_count % 10 == 0):
+                    print_stats()
 except KeyboardInterrupt:
-    printsts(sts, size)
+    print_stats()
     raise
