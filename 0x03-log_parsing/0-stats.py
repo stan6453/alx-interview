0b386893069c46b818f8b1197fa65c240d196f14
stanley <stanley6453@gmail.com>
2023-07-20 14:43:53 +0100

    created file 0x03-log_parsing/0-stats.py

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Parse a log file"""
+
+import sys
+import signal
+
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+
+line_count = 0
+total_file_size = 0
+
+
+def print_stats():
+    print("File size: {}".format(total_file_size))
+    for key, value in status_codes.items():
+        print("{}: {}".format(key, value))
+
+
+try:
+    for line in sys.stdin:
+        line = line[:-1]
+        array = line.split()
+
+        method = str(array[-5][1:])
+        status_code = str(array[-2])
+        file_size = int(array[-1])
+
+        # calc stats
+        total_file_size += file_size
+        status_codes[status_code] += 1
+        line_count += 1
+
+        if (line_count % 10 == 0):
+            print_stats()
+except KeyboardInterrupt:
+    print_stats()
