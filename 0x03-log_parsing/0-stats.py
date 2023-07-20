f96c098182f62a045f90a83b7913edf250ca2a66
stanley <stanley6453@gmail.com>
2023-07-20 16:23:33 +0100

    created file 0x03-log_parsing/0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -25,25 +25,18 @@ def print_stats():
         if value:
             print("{}: {:d}".format(key, value))
 
-
-log_pattern = (r'^(\d+\.\d+\.\d+\.\d+) - \[(\d{4}-\d{2}-\d{2} '
-               r'\d{2}:\d{2}:\d{2}\.\d+)\] "(\w+ [^"]+)" '
-               r'([a-zA-Z0-9]+) (\d+)$'
-               )
-
 try:
     for line in sys.stdin:
         line_count += 1
         line = line[:-1]
         array = line.split()
-        if re.match(log_pattern, line):
-            method = str(array[-5][1:])
-            status_code = str(array[-2])
-            file_size = int(array[-1])
-            if method == "GET" and status_code.isnumeric():
-                # calc stats
-                total_file_size += file_size
-                status_codes[status_code] += 1
+        method = str(array[-5][1:])
+        status_code = str(array[-2])
+        file_size = int(array[-1])
+        if method == "GET" and status_code.isnumeric():
+            # calc stats
+            total_file_size += file_size
+            status_codes[status_code] += 1
 
         if (line_count % 10 == 0):
                     print_stats()
