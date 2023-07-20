ac5b7054489d3f725def5d68ea7de4a7ad90dd3c
stanley <stanley6453@gmail.com>
2023-07-20 14:59:33 +0100

    created file 0x03-log_parsing/0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,7 @@
 """Parse a log file"""
 
 import sys
-import signal
+import re
 
 status_codes = {
     "200": 0,
@@ -25,21 +25,25 @@ def print_stats():
         print("{}: {}".format(key, value))
 
 
+log_pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] "(\w+ [^"]+)" (\d{3}) (\d+)$'
+
+
 try:
     for line in sys.stdin:
         line = line[:-1]
         array = line.split()
+        if re.match(log_pattern, line):
+            method = str(array[-5][1:])
+            status_code = str(array[-2])
+            file_size = int(array[-1])
 
-        method = str(array[-5][1:])
-        status_code = str(array[-2])
-        file_size = int(array[-1])
+            # calc stats
+            total_file_size += file_size
+            status_codes[status_code] += 1
+            line_count += 1
 
-        # calc stats
-        total_file_size += file_size
-        status_codes[status_code] += 1
-        line_count += 1
-
-        if (line_count % 10 == 0):
-            print_stats()
+            if (line_count % 10 == 0):
+                print_stats()
 except KeyboardInterrupt:
     print_stats()
+    sys.exit(0)
