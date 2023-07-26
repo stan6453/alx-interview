1425fd4e5a656cce4c1742d27bae3f09b47388ec
stanley <stanley6453@gmail.com>
2023-07-26 21:45:35 +0100

    implemented utf8-validation algoritm

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,7 +2,8 @@
 """UTF-8 validation module"""
 
 # format -> [bit_mask, value_after_masking, num_of_cont_bits_to_validate]
-head_bit_mask = [[128, 0, 0], [224, 192, 1], [240, 224, 2], [248, 240, 3], 0]
+head_bit_mask = [[128, 0, 0], [224, 192, 1], [240, 224, 2], [248, 240, 3],
+                 [252, 248, 4], [254, 252, 5], 0]
 
 
 def validUTF8(data):
