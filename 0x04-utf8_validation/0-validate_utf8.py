4ea32a43cc6c33c82c9f081ab26fa8dccc3ec7da
stanley <stanley6453@gmail.com>
2023-07-26 20:25:07 +0100

    implemented utf8-validation algoritm

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -20,7 +20,7 @@ def validUTF8(data):
     byte_index = 0
     while byte_index < len(data):
         for item in head_bit_mask:
-            if item == 0:
+            if item == 0 or byte_index >= len(data):
                 return False
             if data[byte_index] & item[0] == item[1]:
                 if not valid_continuation_bits(item[2]):
