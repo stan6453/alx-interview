f70150e24388c5f8f75f6744d17b11320ede4943
stanley <stanley6453@gmail.com>
2023-09-07 16:05:50 +0100

    created a solution to the prime game

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -3,30 +3,38 @@
 isWinner = __import__('0-prime_game').isWinner
 
 # Maria wins
+print('Maria wins')
 print("Winner: {}".format(isWinner(3, [5, 5, 1])))
 print("Winner: {}".format(isWinner(1, [2])))
 print("Winner: {}".format(isWinner(3, [0, 11, 11])))
+print("Winner: {}".format(isWinner(2, [6, 11, 0, 4, 1])))
 
 print('')
 
 # Ben wins
+print('ben wins')
 print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
 print("Winner: {}".format(isWinner(3, [4, 5, 1])))
 print("Winner: {}".format(isWinner(1, [1])))
 print("Winner: {}".format(isWinner(1, [0])))
 print("Winner: {}".format(isWinner(3, [0, 0, 0])))
 print("Winner: {}".format(isWinner(5, [1, 1, 1, 1, 1])))
+print("Winner: {}".format(isWinner(2, [0, 0, 0, 11,11,11])))
+print("Winner: {}".format(isWinner(2, [0, 0, 0, 11,11,11])))
 
 print('')
 
 # None
+print('None')
 print("Winner: {}".format(isWinner(4, [2, 3, 5, 10])))
 print("Winner: {}".format(isWinner(6, [0, 0, 0, 11,11,11])))
-
-
-print("Winner: {}".format(isWinner(2, [0, 0, 0, 11,11,11])))
+print("Winner: {}".format(isWinner(0, [])))
 
 print('')
+
+
+print('wild cases')
+
 
 
 
