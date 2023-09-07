9fb1cce21246e5e11f8fdc059879f4f72dac07b0
stanley <stanley6453@gmail.com>
2023-09-07 15:42:23 +0100

    created a solution to the prime game

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -2,17 +2,22 @@
 
 isWinner = __import__('0-prime_game').isWinner
 
-
-#Maria wins
+# Maria wins
 print("Winner: {}".format(isWinner(3, [5, 5, 1])))
 print("Winner: {}".format(isWinner(1, [2])))
+print("Winner: {}".format(isWinner(3, [0, 11, 11])))
 
+print('')
 
-#Ben wins
+# Ben wins
 print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
 print("Winner: {}".format(isWinner(3, [4, 5, 1])))
 print("Winner: {}".format(isWinner(1, [1])))
+print("Winner: {}".format(isWinner(1, [0])))
+print("Winner: {}".format(isWinner(3, [0, 0, 0])))
+print("Winner: {}".format(isWinner(5, [1, 1, 1, 1, 1])))
 
-#None
+print('')
+
+# None
 print("Winner: {}".format(isWinner(4, [2, 3, 5, 10])))
-
